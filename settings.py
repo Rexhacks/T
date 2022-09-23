a5153ff75093ad0fb710769231d9464a9200fd92
Rexhacks <104732037+Rexhacks@users.noreply.github.com>
2022-09-23 20:22:16 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,24 @@
+Auto_SET_Username = 'yes' #Value must be in yes or no
+
+Auto_Password_set = 'yes' #Value must be in yes or no
+
+Password = 'Your Pass' # Type the Passward which you want to set.
+
+Auto_Remove_InvalidNUmber = 'yes' #Value must be in yes or no
+
+Auto_Remove_BanNUmber = 'yes' #Value must be in yes or no
+
+Auto_Remove_SpamNUmber = 'yes' #Value must be in yes or no
+
+Scrap_ALl_Members = 'yes' #Value Must Be In yes or no
+
+Remove_Promoter_account = 'yes' #Value Must Be In yes or no
+
+Stop_when_work_Done = 'yes' #Value Must Be In yes or no
+
+DeleteAllready_Members = 'yes' #Value Must Be In yes or no
+
+Show_Members_count = 'yes' #Value Must Be In yes or no
+
+
+
